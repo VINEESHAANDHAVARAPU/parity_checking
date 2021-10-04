7c1b67b7e9b57761d294ebf9cc73d140ed809d16
Renu-sri <renuvirigineni14999@gmail.com>
2021-10-04 21:25:10 +0530

    Update parity.c

--- parity.c
+++ parity.c
@@ -1,3 +1,20 @@
 #include<stdio.h>
 #include<string.h>
-
+bool getParity(unsigned int n)
+{
+    bool parity = 0;
+    while (n)
+    {
+        parity = !parity;
+        n      = n & (n - 1);
+    }       
+    return parity;
+}
+int main()
+{
+    unsigned int n = 7;
+    printf("Parity of no %d = %s",  n,
+             (getParity(n)? "odd": "even"));
+    getchar();
+    return 0;
+}
